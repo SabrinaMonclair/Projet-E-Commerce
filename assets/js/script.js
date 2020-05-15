3e94152c280967d18e31ccf869518e2e69edc378
TristanDCP <tristandacostapinto@gmail.com>
2020-05-15 18:14:35 +0200

    osef tier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,38 @@
-
-
-
-
-
 // bouton up page
 let idBtnUpPage = document.getElementById("btnUpPage");
 
-    window.onscroll = function () {
-      if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        idBtnUpPage.style.zIndex = 99;
-        idBtnUpPage.style.opacity = "1";
-        idBtnUpPage.style.bottom = "20px";
-      } else {
-        idBtnUpPage.style.zIndex = -1;
-        idBtnUpPage.style.opacity = "0";
-        idBtnUpPage.style.bottom = "0px";
-      }
-    }
+window.onscroll = function () {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    idBtnUpPage.style.zIndex = 99;
+    idBtnUpPage.style.opacity = "1";
+    idBtnUpPage.style.bottom = "20px";
+  } else {
+    idBtnUpPage.style.zIndex = -1;
+    idBtnUpPage.style.opacity = "0";
+    idBtnUpPage.style.bottom = "0px";
+  }
+}
 
-    function topFunction() {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
+function topFunction() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+// VALUE
+
+let cards = document.querySelector("#cards");
+let userInput = document.getElementsByClassName("inputnumber").value;
+
+
+cards.addEventListener("click", function (event) {
+  if (event.target.tagName.toLowerCase() === 'a') {
+    let clickedItem = event.target;
+    console.log(clickedItem.getAttribute('data-value'));
+    // let commandValue = userInput.value;
+    console.log(userInput);
+  }
+});
+
