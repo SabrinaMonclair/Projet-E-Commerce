881264d4e6627eaadd92a638d6f904d1278015fe
Anthony Le Play <anthony.leplay@gmail.com>
2020-05-15 09:25:36 +0200

    test de code

---
+++ assets/js/jstest.js
@@ -0,0 +1,22 @@
+let idRedwine = document.getElementById("redwine");
+let idzone = document.getElementById("zone");
+let cardNom = ["nom", "img.png", "description", "prix", "ref"]
+
+
+
+idRedwine.onclick = showCardRedWine();
+
+function showCardRedWine() {
+    idzone.inertext = "";
+    idzone.appendChild(createCard(cardRed1));
+
+
+
+
+}
+
+function createCard(type) {
+    idzone
+
+
+}
